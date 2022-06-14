04e950caf2e9d71227bb2f4d1d5b2aa1e7f174a7
alperbicer <alperbicer@yandex.com>
2022-06-14 16:00:59 +0300

    fix(button): remove unused codes

--- src/components/Base/Button.vue
+++ src/components/Base/Button.vue
@@ -1,39 +1,12 @@
 <template>
   <component
     :is="tag"
-    v-bind="buttonAttributeList"
     :type="buttonType"
     class="btn"
     :class="classObject"
     v-on="$listeners"
   >
-    <template v-if="beforeIconName">
-      <BaseIcon
-        :file="beforeIconName"
-        :size="beforeIconSize"
-        :color="beforeIconColor"
-        :badge="beforeIconBadge"
-        :badge-custom-class="beforeIconBadgeCustomClass"
-      />
-    </template>
-    <span
-      v-if="$slots.default && (beforeIconName || afterIconName)"
-      :class="[textClass, customTextClass]"
-    >
-      <slot />
-    </span>
-    <template v-else-if="$slots.default">
-      <slot />
-    </template>
-    <template v-if="afterIconName">
-      <BaseIcon
-        :file="afterIconName"
-        :size="afterIconSize"
-        :color="afterIconColor"
-        :badge="afterIconBadge"
-        :badge-custom-class="afterIconBadgeCustomClass"
-      />
-    </template>
+    <slot />
   </component>
 </template>
 <script>
@@ -54,10 +27,6 @@ export default {
       default: '',
       validator: val => ['', 'xs', 'sm', 'md', 'lg'].includes(val),
     },
-    to: {
-      type: String,
-      default: '',
-    },
     color: {
       type: String,
       default: '',
@@ -70,46 +39,6 @@ export default {
     block: {
       type: Boolean,
       default: false,
-    },
-    beforeIconName: {
-      type: String,
-      default: '',
-    },
-    beforeIconSize: {
-      type: Number,
-      default: null,
-    },
-    beforeIconColor: {
-      type: String,
-      default: '',
-    },
-    beforeIconBadge: {
-      type: [String, Number],
-      default: '',
-    },
-    beforeIconBadgeCustomClass: {
-      type: [String, Array, Object],
-      default: null,
-    },
-    afterIconName: {
-      type: String,
-      default: '',
-    },
-    afterIconSize: {
-      type: Number,
-      default: null,
-    },
-    afterIconColor: {
-      type: String,
-      default: '',
-    },
-    afterIconBadge: {
-      type: [String, Number],
-      default: '',
-    },
-    afterIconBadgeCustomClass: {
-      type: [String, Array, Object],
-      default: null,
     },
     customClass: {
       type: [String, Array, Object],
@@ -130,27 +59,8 @@ export default {
         outline: this.outline ? 'btn-outline' + (this.outline === true ? '' : ' ' + this.outline) : null,
       });
     },
-    textClass() {
-      return {
-        'ml-4': this.$slots.default && this.beforeIconName && (this.size === null || this.size === '' || this.size === 'md'),
-        'ml-2.5': this.$slots.default && this.beforeIconName && (this.size === 'sm' || this.size === 'xs'),
-        'mr-4': this.$slots.default && this.afterIconName && (this.size === null || this.size === '' || this.size === 'md'),
-        'mr-2.5': this.$slots.default && this.afterIconName && (this.size === 'sm' || this.size === 'xs'),
-      };
-    },
     buttonType() {
       return this.tag === 'button' ? this.type : null;
-    },
-    buttonAttributeList() {
-      return {
-        ...(this.tag === 'a' && { href: this.getUrl(this.to) }),
-        ...(this.tag === 'nuxt-link' && { to: this.getUrl(this.to) }),
-      };
-    },
-  },
-  methods: {
-    getUrl(url) {
-      return url && url.charAt(0) === '/' ? url : '/' + (url || '');
     },
   },
 };
